b78cce50f565c4c1b9f281338cde73c022230a57
Eric <eric.tran@korefabrik.com>
2016-10-15 19:51:45 -0700

    fixed nav scroll

--- app/js/header.js
+++ app/js/header.js
@@ -1,28 +1,32 @@
 $(window).scroll(function(event) {
   var scrollPos = $(window).scrollTop();
-  if ( scrollPos > 100) {
+  if ( scrollPos > 150) {
     $(".navbar-brand").hide();
     $("nav").animate({
       height: '10%'
     }, {duration: 400, queue: false});
     $(".navbar-nav").animate({
       height: '50px',
-      marginLeft: '327px'
-    }, {duration: 50, queue: false});
+    }, {duration: 400, queue: false});
+    $(".navbar-nav").animate({
+      marginLeft: "327"
+    }, {duration: 10, queue: false});
      $(".nav-item").animate({
       paddingTop: '15px'
     }, {duration: 400, queue: false});
 
   }
-  if ( scrollPos <= 100) {
+  if ( scrollPos <= 150) {
     $(".navbar-brand").show();
     $("nav").animate({
       height: '100%'
     }, {duration: 400, queue: false });
     $(".navbar-nav").animate({
       height: '100px',
+    }, {duration: 400, queue: false});
+     $(".navbar-nav").animate({
       marginLeft: '0'
-    }, {duration: 50, queue: false});
+    }, {duration: 10, queue: false});
     $(".nav-item").animate({
       paddingTop: '70px'
     }, {duration: 400, queue: false});
