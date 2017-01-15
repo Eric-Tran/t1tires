58772e95973305416cb30b075a53b0ffa22057e8
Eric <eric.tran@korefabrik.com>
2017-01-14 19:17:57 -0800

    under construction page

--- app/components/app.js
+++ app/components/app.js
@@ -6,11 +6,12 @@ export default class App extends Component {
   render() {
     return (
       <div>
-      	<Header />
+      	{ /* <Header /> */ }
       	<div className="body-container">
         	{ /* this.props.children */ }
-          <div>
-            <h2 style={{textTransform: 'none', paddingTop: '100px', textAlign: 'center'}}>Website under construction but feel free to contact us by email <a href="sales@t1tires.com">Sales@T1tires.com</a> or by phone (661) 745-8826."</h2>
+          <div style={{textAlign: 'center'}}>
+            <img src="app/images/tirelogo1.png" style={{width: '40%', border: '3px solid black', borderRadius: '20px'}} alt="T1 Tires" />
+            <h2 style={{textTransform: 'none'}}>Website is currently under construction. Feel free to contact us by email <a href="sales@t1tires.com">Sales@T1tires.com</a> or by phone (661) 745-8826."</h2>
         	</div>
         </div>
       </div>
