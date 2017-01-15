30bdf2626c74287f5caa70ea1c2d6deb81be1a8c
Eric <eric.tran@korefabrik.com>
2017-01-14 20:22:27 -0800

    removed quote

--- app/components/app.js
+++ app/components/app.js
@@ -11,7 +11,7 @@ export default class App extends Component {
         	{ /* this.props.children */ }
           <div style={{textAlign: 'center'}}>
             <img src="app/images/tirelogo1.png" style={{width: '40%', border: '3px solid black', borderRadius: '20px', boxShadow: 'rgba(56, 75, 105, .5) 0px 6px 9px 0px'}} alt="T1 Tires" />
-            <h2 style={{textTransform: 'none'}}>Website is currently under construction. Feel free to contact us by email <a href="sales@t1tires.com">Sales@T1tires.com</a> or by phone (661) 745-8826."</h2>
+            <h2 style={{textTransform: 'none'}}>Website is currently under construction. Feel free to contact us by email <a href="sales@t1tires.com">Sales@T1tires.com</a> or by phone (661) 745-8826.</h2>
         	</div>
         </div>
       </div>
