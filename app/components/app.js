e28f5aa75969c05bdf656e903dc449df6cca8664
Eric <eric.tran@korefabrik.com>
2017-01-14 19:30:00 -0800

    boxshadow

--- app/components/app.js
+++ app/components/app.js
@@ -10,7 +10,7 @@ export default class App extends Component {
       	<div className="body-container">
         	{ /* this.props.children */ }
           <div style={{textAlign: 'center'}}>
-            <img src="app/images/tirelogo1.png" style={{width: '40%', border: '3px solid black', borderRadius: '20px'}} alt="T1 Tires" />
+            <img src="app/images/tirelogo1.png" style={{width: '40%', border: '3px solid black', borderRadius: '20px', boxShadow: 'rgba(56, 75, 105, .5) 0px 6px 9px 0px'}} alt="T1 Tires" />
             <h2 style={{textTransform: 'none'}}>Website is currently under construction. Feel free to contact us by email <a href="sales@t1tires.com">Sales@T1tires.com</a> or by phone (661) 745-8826."</h2>
         	</div>
         </div>
